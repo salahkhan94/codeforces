fb3ab725475526e625c2f63ca10d6b81707ab0fe
salahkhan94 <salahuddin.khan.1994@gmail.com>
2020-05-06 01:56:35 +0530

    bitsets

---
+++ bitwiseformula.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    unsigned int n,m,q=0;
+    string rawInput;
+    getline(cin, rawInput,' ');
+    n = stoi(rawInput.c_str());
+    getline(cin, rawInput);
+    m = stoi(rawInput.c_str());
+    
+
+}
